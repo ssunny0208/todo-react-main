bcc9c88d417f1d79fecac2cd752aa990292e87c0
ssunny0208 <xdfv635@snu.ac.kr>
2024-04-30 12:03:53 +0900

    dd

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,73 +1,21 @@
-import React, { useState, useEffect } from "react";
-import { db } from "@firebase";
-import {
-  collection,
-  query,
-  getDocs,
-  addDoc,
-  updateDoc,
-  deleteDoc,
-} from "firebase/firestore";
-import TodoItem from "@/components/TodoItem";
-import styles from "@styles/TodoList.module.css";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-const todoCollection = collection(db, "todos");
-
-const TodoList = () => {
-  const [todos, setTodos] = useState([]);
-  const [input, setInput] = useState("");
-
-  const getTodos = async () => {
-    const q = query(todoCollection);
-    const results = await getDocs(q);
-    const newTodos = results.docs.map((doc) => ({
-      id: doc.id,
-      ...doc.data(),
-    }));
-    setTodos(newTodos);
-  };
-
-  useEffect(() => {
-    getTodos();
-  }, []);
-
-  const addTodo = async () => {
-    if (input.trim() === "") return;
-    const docRef = await addDoc(todoCollection, {
-      text: input,
-      completed: false,
-    });
-    setTodos([...todos, { id: docRef.id, text: input, completed: false }]);
-    setInput("");
-  };
-
-  const toggleTodo = (id) => {
-    setTodos(
-      todos.map((todo) => {
-        if (todo.id === id) {
-          const todoDoc = doc(todoCollection, id);
-          updateDoc(todoDoc, { completed: !todo.completed });
-          return { ...todo, completed: !todo.completed };
-        } else {
-          return todo;
-        }
-      })
-    );
-  };
-
-  const deleteTodo = (id) => {
-    const todoDoc = doc(todoCollection, id);
-    deleteDoc(todoDoc);
-    setTodos(todos.filter((todo) => todo.id !== id));
-  };
-
-  // TodoItem 컴포넌트의 사용 예시 등 추가 구현이 필요할 수 있습니다.
-
-  return (
-    <div className={styles.todoList}>
-      {/* 여기에 TodoItem을 렌더링하는 로직 추가 */}
-    </div>
-  );
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCkEYtVqCNnNpggOsFK-XM7g_rX24ew7Rs",
+  authDomain: "todo-69010.firebaseapp.com",
+  projectId: "todo-69010",
+  storageBucket: "todo-69010.appspot.com",
+  messagingSenderId: "78948333732",
+  appId: "1:78948333732:web:d4777e2d4f3e8264d58e31",
+  measurementId: "G-0EEHY6H35W",
 };
 
-export default TodoList;
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
