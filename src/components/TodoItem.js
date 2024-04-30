5848be1ebde9fc431e256ee9c65db021f7874c65
ssunny0208 <xdfv635@snu.ac.kr>
2024-04-30 12:36:49 +0900

    04301

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -25,7 +25,7 @@ const TodoItem = ({ todo, onToggle, onDelete }) => {
       </span>
 
       {/* 삭제 버튼을 렌더링하고, 클릭 시 onDelete 함수를 호출하여 해당 할 일을 삭제합니다. */}
-      <button onClick={onDelete}>지우기</button>
+      <button onClick={onDelete}>Delete</button>
     </li>
   );
 };
