481b1c7ba8e45f7f85435b5a7ff94d666bc153da
ssunny0208 <xdfv635@snu.ac.kr>
2024-04-30 14:15:19 +0900

    04302

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
 import TodoList from "@/components/TodoList";
+import styles from "@/styles/globals.css"; // 필요한 경우 import
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.centerContainer}>
+      <h1 className={styles.title}>To Do List</h1>
       <TodoList />
     </div>
   );
