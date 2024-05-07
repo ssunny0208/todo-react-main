0f47e7fcdf145ab59ba547703ee8e301d8d6497b
ssunny0208 <xdfv635@snu.ac.kr>
2024-05-07 11:56:17 +0900

    kakao

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,15 @@
+"use client";
+
+import { SessionProvider } from "next-auth/react";
+
 import TodoList from "@/components/TodoList";
-import styles from "@/styles/globals.css"; // 필요한 경우 import
 
 export default function Home() {
   return (
-    <div className={styles.centerContainer}>
-      <h1 className={styles.title}>To Do List</h1>
-      <TodoList />
-    </div>
+    <SessionProvider>
+      <div>
+        <TodoList />
+      </div>
+    </SessionProvider>
   );
 }
